b112c93fe5ee0414c346ef686c6fa35f27cbb419
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-07-13 16:08:53 +0900

    added CORS

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Expose-Headers', 'x-auth');
+  res.header('Access-Control-Allow-Headers', 'Origin, x-auth, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, DELETE, OPTIONS');
+  next();
+});
+
 app.post('/', (req, res) => {
   try {
     const transporter = nodemailer.createTransport({
